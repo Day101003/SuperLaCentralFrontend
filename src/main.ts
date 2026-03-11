0a40eb0e8af483f59b5520279be5fa266e490b7f
grimaldi <solanogrimaldi09@gmail.com>
2026-03-11 13:17:37 -0600

    avance merce con side bar y en el main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -12,9 +11,7 @@ import { authInterceptor } from './core/interceptors/auth.interceptor';
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
-    provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     importProvidersFrom(FormsModule)
   ]
 }).catch((err) => console.error(err));
